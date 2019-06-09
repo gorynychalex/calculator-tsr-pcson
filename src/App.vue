632ab239ed0c3aee3da38c0dd7e904aa55573e95
gorynychalex <gorynychalex@gmail.com>
2019-06-09 13:29:16 +1000

    equipments component added

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo_pcson.png">
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <Equipment/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Equipment from './components/Equipment.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Equipment
   }
 }
 </script>
---
+++ src/components/Equipment.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <b-form-group id="equipments" label="Средства реабилитации:" label-for="reabilitaionEquipment">
+
+            <b-form-select id="reabilitaionEquipment" v-model="selected" :options="equipmentList" text-field="name" value-field="price" required></b-form-select>
+
+            <div class="mt-3">Selected: <strong>{{ selected }}</strong></div>
+
+        </b-form-group>
+    </div>
+</template>
+
+<script>
+    import equipments from '@/data/tsr-equipment.json'
+
+    export default {
+        name: "Equipment",
+        props: {
+
+        },
+        data() {
+            return {
+                equipments,
+                equipmentList: [ {"id": null, "name": "Выберите средство ...", "price": null}, ...equipments],
+                selected: null
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
